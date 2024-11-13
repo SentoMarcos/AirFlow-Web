24a6482d297ad37f02a52fe59eab6c45d104d223
Ruby García <rgarqui@epsg.upv.es>
2024-11-13 17:30:29 +0100

    Login v1

---
+++ public/js/header.js
@@ -0,0 +1,7 @@
+// Cargar el header externo
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error al cargar el header:', error));
