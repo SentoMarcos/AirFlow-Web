1db2025a503c9ee52390733c995994e9f5bfa42e
Ruby García <rgarqui@epsg.upv.es>
2024-11-14 12:59:07 +0100

    Css v2

--- public/js/header.js
+++ public/js/header.js
@@ -3,5 +3,17 @@ fetch('header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-container').innerHTML = data;
+
+        // Selecciona el header
+        const header = document.querySelector('.container-fluid');
+
+        // Agrega un evento para detectar el scroll
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 0) {
+                header.classList.add('scrolled');
+            } else {
+                header.classList.remove('scrolled');
+            }
+        });
     })
     .catch(error => console.error('Error al cargar el header:', error));
