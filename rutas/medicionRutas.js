e21773a118239e60581799037651f03ce1b23cad
smariba <smaribar@epsg.upv.es>
2024-12-12 13:28:15 +0100

    send mediciones

--- rutas/medicionRutas.js
+++ rutas/medicionRutas.js
@@ -8,17 +8,20 @@
  */
 
 const express = require('express');
-const { createMedicion } = require('../controladores/medicionControlador');
+const medicionControlador = require('../controladores/medicionControlador');
 const router = express.Router();
 
 /**
  * Rutas de Mediciones
  * @POST - Crear una nueva medición
- *
+ * @GET - Obtener todas las mediciones
+ * @GET - Obtener todas las mediciones de un sensor
  * @module medicionRutas
  */
 // Ruta para crear una nueva medición
-router.post('/mediciones', createMedicion);
+router.post('/mediciones/add', medicionControlador.createMedicion);
+router.get('/mediciones/:id', medicionControlador.getMedicionesOfSensor);
+router.get('/mediciones', medicionControlador.getAllMediciones);
 
 // Aquí puedes agregar más rutas para Mediciones
 
